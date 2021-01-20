7f74793b005c3876295b92eee2fe819065221a40
Johnsonrobot <b10413157@yuntech.edu.tw>
2021-01-20 21:46:39 +0800

    determine user & pwd data

--- action.js
+++ action.js
@@ -4,3 +4,12 @@ function login(){
 function register(){
     
 }
+function getObject(id){
+    return typeof id == 'object' ? id : document.getElementById(id);
+}
+function getStyle(id){
+    return object(id).style
+}
+function getClass(id){
+    return document.getElementsByClassName(id);
+}
--- register.php
+++ register.php
@@ -1,7 +1,5 @@
 <?php
     require_once 'register.html';
     require_once 'db.php';
-    if(isset($_POST['register'])){
-        
-    }
+    
 ?>
