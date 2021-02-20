f2abcc5d53d40ded9632a1d074fa852459c09a4c
Johnsonrobot <b10413157@yuntech.edu.tw>
2021-02-20 21:55:25 +0800

    implement login verify

--- login.php
+++ login.php
@@ -1,21 +1,17 @@
 <?php
     require_once 'login.html';
     require_once 'db.php';
-    $flag = $msg = $user = $pwd = "";
-    // if(isset($_POST['user'])){
-    //     $user = sanitizeString($_POST['user']);
-    //     $pwd = sanitizeString($_POST['pwd']);
-    //     $pwd = password_hash($pwd, PASSWORD_DEFAULT);
-    //     $result = query_mysql("select * from register_data where username='$user'");
+    $flag = $msg = $user = $pwd = $pwd_hash = "";
+    if(isset($_POST['user'])){
+        $user = sanitizeString($_POST['user']);
+        $pwd = sanitizeString($_POST['pwd']);
+        $pwd_hash = password_hash($pwd, PASSWORD_DEFAULT);
+        $result = query_mysql("select * from register_data where username='$user'");
 
-    //     if($result->num_rows){
-    //         $flag = "false";
-    //         header("Location:register.php?$flag");
-    //     }else{
-    //         add_user($user, $pwd);
-    //         // query_Mysql("insert into register_data values('$user', '$pwd')");
-    //         $flag = "true";
-    //         header("Location:index.html?$flag");
-    //     }
-    // }
+        if($result->num_rows){
+            echo "Login success";
+        }else{
+            echo "Login failed";
+        }
+    }
 ?>
